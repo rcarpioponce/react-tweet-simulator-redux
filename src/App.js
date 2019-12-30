903eecda4bae97a4d4e273896942feb61b9580f6
Renzo Carpio <me@renzocarpio.com>
2019-12-30 10:20:23 -0500

    se creo el componente menu y se agrego el boton de agregar tweet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+
+import Menu from "./components/Menu";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>TWEETS</h1>
-    </div>
+    <>
+      <Menu />
+      <Container className="mt-5">
+        <h1 class="text-center">TWEETS</h1>
+      </Container>
+    </>
   );
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, Navbar, Button } from "react-bootstrap";
+
+import LogoRedux from "../assets/img/redux.png";
+
+export default function Menu() {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand>
+          <img
+            alt="Tweets simulator redux"
+            src={LogoRedux}
+            width="30"
+            height="30"
+            className="d-inline-block align-top mr-4"
+          />
+          Tweets Simulator Redux
+        </Navbar.Brand>
+        <Button variant="outline-success">Nuevo Tweet</Button>
+      </Container>
+    </Navbar>
+  );
+}
